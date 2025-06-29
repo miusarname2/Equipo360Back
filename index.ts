178c0fc3398f4a2a6f2d9b0567a77fa8c76502ce
miusarnameBotslovers <oscar.develop@botslovers.com>
2025-06-29 00:23:36 -0500

    feat: :sparkles: Se crea un "HealthCheck"

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log('hola mungo');
+import express, { Express, Request, Response } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./" });
+
+// Create an Express application
+const app: Express = express();
+
+// Setup port
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors({ origin: "*" }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + Ts  server⚡️!");
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
